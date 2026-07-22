8816f82e439a9e35c5d5ec6fad9c0f249a067bea
Alex-OS-CR-7 <martys_a_v@mail.ru>
2026-07-22 15:42:37 +0300

    Подключен модуль random. Добавлены строковые константы. Добавлена переменная для хранения доступных к использованию символов.

---
+++ secure_password_generator_git_project.py
@@ -0,0 +1,12 @@
+import random as r
+
+digits = '0123456789'
+
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+
+uppearcase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+punctuation = '!#$%&*+-=?@^_'
+
+chars = ''
+
